604d708878b0fbd53302a9e33fc4ef003d1eba50
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-05-20 20:56:43 +0100

    wrote dummy test case for now on exercise006.

--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -1,5 +1,10 @@
 import {
     sumMultiples,
+    isValidDNA,
+    getComplementaryDNA,
+    isItPrime,
+    createMatrix,
+    areWeCovered,
 
 } from '../challenges/exercise006';
 
@@ -7,4 +12,34 @@ describe("sumMultiples", () => {
     test("add the sum of arrays", () => {
         expect(sumMultiples([1, 2, 8, 13, 7])).toBe(0);
     })
-})
+});
+
+describe("isValidDNA", () => {
+    test("checks for DNA", () => {
+        expect(isValidDNA()).toBe();
+    })
+});
+
+describe("getComplementaryDNA", () => {
+    test("get DNA", () => {
+        expect(getComplementaryDNA()).toBe();
+    })
+});
+
+describe("isItPrime", () => {
+    test("is it a prime number return true or else false", () => {
+        expect(isItPrime()).toBe()
+    })
+});
+
+describe("createMatrix", () => {
+    test("create a matrix", () => {
+        expect(createMatrix()).toBe()
+    })
+});
+
+describe("areWeCovered", () => {
+    test("cover all the areas", () => {
+        expect(areWeCovered()).toBe()
+    })
+});
